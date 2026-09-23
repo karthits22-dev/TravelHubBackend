3f206f0864e266bc68d7767a018e6fbf7a481f9c
cvibe karthi <cvibekarthi@gmail.com>
2026-09-23 21:47:37 +0530

    configure production server

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser,getUsers,deleteUser,updateUser,loginUser } = require('../controllers/userController');
+const { registerUser,getUsers,deleteUser,updateUser,loginUser } = require('../Controllers/userController');
 
 
 
